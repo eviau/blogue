2f6f361f76938dff461c429cc3ea5ead7c024b72
eviau <info@eviau.net>
2022-08-13 06:50:33 -0400

    more github actions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,14 +14,12 @@ release = ''
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinxcontrib.newsfeed']
 
 templates_path = ['_templates']
 exclude_patterns = []
 
 language = 'fr'
-
-extensions.append('sphinxcontrib.newsfeed')
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
